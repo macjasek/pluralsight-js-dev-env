a2437cdaf0f4f14d74f705e1a0f8caf40421c5ec
macjasek <maciej.dobruchowski@ispro.com.pl>
2017-11-09 19:49:50 +0100

    dynamic html, minify, bundle spliting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,18 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
     devtool: 'source-map',
-    entry: './src/index',
+    // entry: './src/index',
+    entry: {
+        vendor: path.resolve(__dirname, 'src/vendor'),
+        main: path.resolve(__dirname,'src/index')
+    },
     target: 'web',
     output: {
         path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js',
+        filename: '[name].js',
         publicPath: '/'
     },
     module: {
@@ -18,6 +23,28 @@ export default {
     },
 
     plugins: [
+        // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
+        // Create HTML file that includes reference to bundled JS.
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+              },
+            inject: true
+        }),
         //Elimitate duplicate packages when generatin bundle
         new webpack.optimize.DedupePlugin(),
 

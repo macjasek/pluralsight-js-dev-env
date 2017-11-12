a580fa47cd1ac81baf03f542639dc0e16a1ba734
macjasek <maciej.dobruchowski@ispro.com.pl>
2017-11-12 11:40:54 +0100

    build and deploy

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WEbpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
     devtool: 'source-map',
@@ -12,22 +14,40 @@ export default {
     target: 'web',
     output: {
         path: path.resolve(__dirname,'dist'),
-        filename: '[name].js',
+        filename: '[name].[chunkhash].js',
         publicPath: '/'
     },
     module: {
         rules: [
             {test: /\.js$/,exclude: /node_modules/, loaders: ['babel-loader']},
-            {test: /\.css$/,use:[{loader: "style-loader"},{loader: "css-loader", options: {modules: true}}]}
+            //{test: /\.css$/,use:[{loader: "style-loader"},{loader: "css-loader", options: {modules: true}}]},
+            {test:/\.css$/, use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: "css-loader",
+                        publicPath: "/dist"
+                      })
+            }
+            //{test: /\.css$/,use:[{loader: ExtractTextPlugin.extract('css?sourceMap'), options: {modules: true}}]}
+            //{test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
         ]
     },
 
     plugins: [
+        // Generate an external css file with a hash in the filename
+        new ExtractTextPlugin({
+                filename: '[name].[contenthash].css',
+                disable: false,
+                allChunks: true
+              }),
+
+        // Hash the files using MD5 so that their names change when the content changes.
+        new WEbpackMd5Hash(),
+
         // Use CommonsChunkPlugin to create a separate bundle
-    // of vendor libraries so that they're cached separately.
-    new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor'
-      }),
+        // of vendor libraries so that they're cached separately.
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
         // Create HTML file that includes reference to bundled JS.
         new HtmlWebpackPlugin({
             template: 'src/index.html',
@@ -45,8 +65,6 @@ export default {
               },
             inject: true
         }),
-        //Elimitate duplicate packages when generatin bundle
-        new webpack.optimize.DedupePlugin(),
 
         //Minify JS
         new webpack.optimize.UglifyJsPlugin()
